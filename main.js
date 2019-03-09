20eb24dfdf8677097603a1edb0ca502bddb9c448
sarahjulesthorne <sarah.jules.thorne@gmail.com>
2019-03-08 20:12:41 -0600

    pushing working project cards

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const projects = [
     {
 title: 'HTML project',
-screenshot: '../image-test/2019-03-06.png',
+screenshot: './2019-03-06.png',
 description: 'This project is an exercise in basic HTML. I created it to practice HTmL tags, and I did so by writing an HTmL document with various tags such as sections and headers.',
 technologiesUsed: 'HTML, CSS, vanilla JavaScript, version control with Github, Ubuntu terminal.',
 available: true,
@@ -11,16 +11,16 @@ githubURL: 'www.github.com/sarahjulesthorne',
     },
     {
         title: 'CSS Project',
-        screenshot: '../image-test/2019-03-06.png',
+        screenshot: './2019-03-06.png',
         description: 'This project is an exercise in styling using CSS. I created it to practice flexbox and color manipulation. I used various styles such as display, background-color, and flex-flow to reach my outcome.',
         technologiesUsed: 'HTML, CSS, vanilla JavaScript, version control with Github, Ubuntu terminal.',
-        available: false;
+        available: false,
         url: 'www.github.com/sarahjulesthorne',
         githubURL: 'www.github.com/sarahjulesthorne',
             },
             {
                 title: 'JavaScript Project',
-                screenshot: '../image-test/2019-03-06.png',
+                screenshot: './2019-03-06.png',
                 description: 'This project is an exercise in JavaScript functions. I created it to practice looping, variables, and functions, etc. I reached my goal by practicing creating arrays, looping over them with for loops, and printing to the dom.',
                 technologiesUsed: 'HTML, CSS, vanilla JavaScript, version control with Github, Ubuntu terminal.',
                 available: true,
@@ -29,7 +29,7 @@ githubURL: 'www.github.com/sarahjulesthorne',
                     },
                     {
                         title: 'Github Project',
-                        screenshot: '../image-test/2019-03-06.png',
+                        screenshot: './2019-03-06.png',
                         description: 'This project is an exercise in version control with Github. I created it to master repository basics in Github. I did so by creating, initializing, adding, commiting, and pushing repositories and files to Github. I also practiced branch creation and merging/pull requesting in the branches.',
                         technologiesUsed: 'HTML, CSS, vanilla JavaScript, version control with Github, Ubuntu terminal',
                         available: false,
@@ -48,24 +48,20 @@ selectedDiv.innerHTML = textToPrint;
 let domString = '';
 const createProjectCards = () => {
 for (let i = 0; i < projects.length; i++) {
-domString += `</div class='project-card'>`;
-domString += `<header>`;
+    if (projects[i].available) {
+domString += `</section class='project-card'>`;
 domString += `<h5 class='card-title'>${projects[i].title}</h5>`;
-domString += `</header>`;
 domString += `<img src=${projects[i].screenshot} alt='screenshot of project page'/>`;
-domString += `<p>${projects[i].description}</p>`;
-domString += `<p>${projects[i].technologiesUsed}</p>`;
+domString += `<p>Project Description: ${projects[i].description}</p>`;
+domString += `<p>Technologies Used: ${projects[i].technologiesUsed}</p>`;
 domString += `<footer>`;
 domString += `<p>View the project <a href=projects[i].url>here</a></p>`;
 domString += `<p>View the project on Github <a href=projects[i].githubURL>here</a></p>`;
 domString += `</footer>`;
 domString += `</section`;
-if (projects[i].available === true) {
-printToDom('projectsPage', domString);
+    }
 }
-}
-    
-
+printToDom('projectsContainer', domString);
 };
 const init = () => {
 createProjectCards();
